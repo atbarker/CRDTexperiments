172a042ed175e0a3fce3ac58793a280f814276b4
austen barker <atbarker@ucsc.edu>
2018-10-27 17:18:40 -0700

    skeleton

---
+++ op.py
@@ -0,0 +1,14 @@
+
+
+class opLWWRegister(object):
+
+    def __init__(self, client_id=None):
+        pass
+
+    def payload(self):
+        pass
+
+    def update(self):
+        pass
+
+
---
+++ sets.py
@@ -0,0 +1,18 @@
+import base64
+
+class stateGSet(object):
+
+
+class state2PSet(object):
+
+
+class op2PSet(object):
+
+
+
+class MWSSet(object):
+
+
+class opORSet(object):
+
+
