6328607e8f60e67de6dc98ad4403dc253bd04404
austen barker <atbarker@ucsc.edu>
2018-10-29 20:40:53 -0700

    op based options

--- Gset.go
+++ Gset.go
@@ -1,11 +1,21 @@
 package Gset
 
+import "container/list"
+
 //map interfaces (key) to structs (value) in our set
 type baseSet map[interface{}]struct{}
 
 //all our Gset has to contain is a single set that grows monotonically
 type Gset struct {
 	baseSet baseSet
+}
+
+
+//used to contain operations that are then sent to another 
+type OpList struct {
+	Operation string
+	Element   interface{}
+	contents  struct{}
 }
 
 func NewGset() *Gset {
@@ -33,6 +43,19 @@ func (g *Gset) List()  ([]interface{}, error){
 
 func (g *Gset) Length() (int, error){
 	return len(g.baseSet), nil
+}
+
+func (g *Gset) ApplyOps(opslist *list.List) error {
+	for e := opslist.Front(); e != nil; e = e.Next() {
+		oplistElement := e.Value.(*OpList)
+		if oplistElement.Operation == "Add" {
+			g.Add(oplistElement.Element)
+			g.baseSet[oplistElement.Element] = oplistElement.contents
+		}else{
+			return nil
+		}
+	}
+	return nil
 }
 
 
